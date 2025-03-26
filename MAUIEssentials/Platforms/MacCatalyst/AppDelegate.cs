43fcac2f2bcdf477d8431ac21fa389464b03ad32
Ankita Patel <Ankita@mobmaxime.com>
2025-03-26 11:44:04 +0530

    Create project

---
+++ MAUIEssentials/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MAUIEssentials;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
